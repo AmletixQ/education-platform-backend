33390c62b95249aefc87dfa8922325bd093c157e
AmletixQ <nazarovamridin308@gmail.com>
2025-01-29 15:13:07 +0300

    Some fix & additionals

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 4200);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
